06bcce895b557d1161b3d1871da2ee9ed5a8435d
AcePeQ <126714703+AcePeQ@users.noreply.github.com>
2024-05-03 10:45:05 +0200

    Create playRound function
    The function is responsible to play one round of the game, function takes 2 parameters, string with humand and computer choises

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let computerChoice;
   const randomNumber = Math.floor(Math.random() * 3);
@@ -20,5 +23,53 @@ function getComputerChoice() {
   return computerChoice;
 }
 
+function getHumanChoice() {
+  const userChoice = prompt(
+    "What's your choice? Choice between: paper, rock, scissors",
+    "paper"
+  ).toLowerCase();
+
+  if (!userChoice)
+    return window.alert("You need to choice between paper, rock, scissors");
+
+  return userChoice;
+}
+
+function playRound(userChoice, computerChoice) {
+  switch (userChoice) {
+    case "rock":
+      if (computerChoice === "scissors") {
+        console.log("You won! Rock beats Scissors");
+        humanScore++;
+      } else if (computerChoice === "paper") {
+        console.log("You lost! Paper beats Rock");
+        computerScore++;
+      }
+      break;
+
+    case "paper":
+      if (computerChoice === "rock") {
+        console.log("You won! Paper beats Rock");
+        humanScore++;
+      } else if (computerChoice === "scissors") {
+        console.log("You lost! Scissors beats Paper");
+        computerScore++;
+      }
+      break;
+
+    case "scissors":
+      if (computerChoice === "paper") {
+        console.log("You won! Scissors beats paper");
+        humanScore++;
+      } else if (computerChoice === "rock") {
+        console.log("You lost! Rock beats Scissors");
+        computerScore++;
+      }
+      break;
+  }
+}
+
 const computerChoice = getComputerChoice();
-console.log(computerChoice);
+const userChoice = getHumanChoice();
+
+playRound(userChoice, computerChoice);
