d42b0a249b16f375d112030190aeae326a06014a
AcePeQ <126714703+AcePeQ@users.noreply.github.com>
2024-05-03 10:17:56 +0200

    Create getComputerChoice function
    Function generate number between 0 and 2, and that number is used as a parameter for switch which reasign computerChoice variable to be a value to return

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+function getComputerChoice() {
+  let computerChoice;
+  const randomNumber = Math.floor(Math.random() * 3);
+
+  switch (randomNumber) {
+    case 0:
+      computerChoice = "rock";
+      break;
+    case 1:
+      computerChoice = "paper";
+      break;
+    case 2:
+      computerChoice = "scissors";
+    default:
+      break;
+  }
+
+  return computerChoice;
+}
+
+const computerChoice = getComputerChoice();
+console.log(computerChoice);
